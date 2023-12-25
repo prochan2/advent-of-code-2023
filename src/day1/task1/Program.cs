194c0604dafa84d8c027127167ad6fdffc592904
Antonín Procházka <antonin@postsharp.net>
2023-12-25 14:54:13 +0100

    Day1 Task1

--- src/day1/task1/Program.cs
+++ src/day1/task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines(Path.Combine("..", "..", "..", "input",
+    //"sinput.txt"));
+    "input.txt"));
+
+//lines.ToList().ForEach(Console.WriteLine);
+//Console.WriteLine();
+
+var digitLines = lines.Select(l => l
+    .Where(char.IsDigit)
+    .Select(c => (long)(c - '0')).ToArray());
+
+//digitLines.ToList().ForEach(Console.WriteLine);
+//Console.WriteLine();
+
+var calibrationValues = digitLines.Select(d => d[0] * 10 + d[^1]);
+
+//calibrationValues.ToList().ForEach(Console.WriteLine);
+//Console.WriteLine();
+
+var sum = calibrationValues.Sum();
+
+Console.WriteLine(sum);
